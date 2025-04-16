182d1ba96d8a6e5cae5a4d798445c0e34ed0112e
Akshay Patel <patelsinghakshay30@gmail.com>
2025-04-16 19:32:22 +0530

    fetching salon details inside create appointment sidebar

--- src/components/subcomponents/calendarView.jsx
+++ src/components/subcomponents/calendarView.jsx
@@ -5,8 +5,8 @@ import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-import Sidebar from '../subcomponents/SideServiceSlider'; 
-import FetchSalonService from '../subcomponents/FetchSalonServices'
+import Sidebar from './SideServiceSlider'; 
+import FetchSalonService from './FetchSalonServices'
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
